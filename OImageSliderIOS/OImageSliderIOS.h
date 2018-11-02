4c07ff3f1c8da139eb80eff24b9f33ab9aa52b72
mac <ouailbellal@gmail.com>
2018-11-03 00:53:17 +0100

    Initial Commit

---
+++ OImageSliderIOS/OImageSliderIOS.h
@@ -0,0 +1,19 @@
+//
+//  OImageSliderIOS.h
+//  OImageSliderIOS
+//
+//  Created by mac on 11/3/18.
+//  Copyright © 2018 ouail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OImageSliderIOS.
+FOUNDATION_EXPORT double OImageSliderIOSVersionNumber;
+
+//! Project version string for OImageSliderIOS.
+FOUNDATION_EXPORT const unsigned char OImageSliderIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OImageSliderIOS/PublicHeader.h>
+
+
